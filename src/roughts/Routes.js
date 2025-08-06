4dc09a0d96504ffc59ab07b2719d9c0f8cbf33bb
Tukaram Todkari <tukatodkari@gmail.com>
2025-08-07 02:29:48 +0530

    Added homepage in the package.json and fixed typo

--- src/roughts/Routes.js
+++ src/roughts/Routes.js
@@ -13,7 +13,6 @@ const AppRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
